52445bf97ef10a25d8944858b0d3671303c583ec
truongnqse05461 <truong10011998@gmail.com>
2020-06-09 15:04:46 +0700

    Initial commit

---
+++ src/main/java/com/example/fptufindingmotelv1/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.fptufindingmotelv1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getHomepage(Model model){
+        return "index";
+    }
+    @GetMapping("/dang-nhap")
+    public String getLogin(Model model){
+        return "login";
+    }
+}
---
+++ src/test/java/com/example/fptufindingmotelv1/FptuFindingMotelV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fptufindingmotelv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FptuFindingMotelV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

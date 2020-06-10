586413d9ce6d82243c76f1430c5db3d771ff04c3
DatCTSE06043 <datctse06043@fpt.edu.vn>
2020-06-10 15:23:41 +0700

    cmm

--- src/main/java/com/example/fptufindingmotelv1/repository/UserRepository.java
+++ src/main/java/com/example/fptufindingmotelv1/repository/UserRepository.java
@@ -1,10 +1,9 @@
 package com.example.fptufindingmotelv1.repository;
 
-import com.example.fptufindingmotelv1.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository {
 
 }

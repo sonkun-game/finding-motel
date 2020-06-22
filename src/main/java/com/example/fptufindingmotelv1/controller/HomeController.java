065a1736f36e34ac642261614cb90ceb7134cb36
DatCTSE06043 <datctse06043@fpt.edu.vn>
2020-06-22 16:16:24 +0700

    Wishlish add + delete

--- src/main/java/com/example/fptufindingmotelv1/controller/HomeController.java
+++ src/main/java/com/example/fptufindingmotelv1/controller/HomeController.java
@@ -1,16 +1,23 @@
 package com.example.fptufindingmotelv1.controller;
+import com.example.fptufindingmotelv1.dto.PostDTO;
+import com.example.fptufindingmotelv1.model.CustomUserDetails;
 import com.example.fptufindingmotelv1.model.PagerModel;
 import com.example.fptufindingmotelv1.model.PostModel;
+import com.example.fptufindingmotelv1.model.RenterModel;
 import com.example.fptufindingmotelv1.repository.PostModelRepository;
 import com.example.fptufindingmotelv1.service.displayall.PostService;
 import com.example.fptufindingmotelv1.service.displayall.RenterService;
 import com.example.fptufindingmotelv1.untils.Constant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 @Controller
 public class HomeController {
@@ -34,37 +41,38 @@ public class HomeController {
         if (sort.equals("ASC")) {
             sortable = Sort.by("createDate").ascending();
         }
-
         int evalPageSize = size.orElse(Constant.INITIAL_PAGE_SIZE);
         int evalPage = (page.orElse(0) < 1) ? Constant.INITIAL_PAGE : page.get() - 1;
         Pageable pageable = PageRequest.of(evalPage, evalPageSize,sortable);
         Page<PostModel> postList =  postModelRepository.findAll(pageable);
 
-        /*List<WishlistDTO> wishListDTOs= new ArrayList<>();
+        List<PostDTO> postDTOs= new ArrayList<>();
+        PostDTO postDTO= null;
         if(SecurityContextHolder.getContext().getAuthentication() instanceof UsernamePasswordAuthenticationToken){
             CustomUserDetails userDetails = (CustomUserDetails)SecurityContextHolder.getContext()
                     .getAuthentication().getPrincipal();
             RenterModel renter =renterService.findOne(userDetails.getUserModel().getUsername());
-            WishlistDTO wishlistDTO;
             for(int j=0;j<postList.getSize();j++){
-                wishlistDTO=new WishlistDTO(postList.getContent().get(j));
-                for(int i=0;i< renter.getPosts().size();i++){
-                    if(postList.getContent().get(j).getId() == renter.getPosts().get(i).getId()){
-                        wishlistDTO.setColor("color: red");
-                    }else {
-                        wishlistDTO.setColor("color: white");
-                    }
+                postDTO=new PostDTO(postList.getContent().get(j));
+                if(renter.getPosts().contains(postList.getContent().get(j))){
+                    postDTO.setColor("color: red");
+                }else {
+                    postDTO.setColor("color: white");
                 }
-                wishListDTOs.add(wishlistDTO);
+                postDTOs.add(postDTO);
+            }
+        }else{
+            for (int i=0;i<postList.getSize();i++){
+                postDTO = new PostDTO(postList.getContent().get(i));
+                postDTOs.add(postDTO);
             }
         }
-        Page<WishlistDTO> listDTO = new PageImpl<>(wishListDTOs);*/
-        model.addAttribute("posts",postList);
-        PagerModel pager = new PagerModel(postList.getTotalPages(),postList.getNumber(),Constant.BUTTONS_TO_SHOW);
+        Page<PostDTO> listDTO = new PageImpl<>(postDTOs);
+        model.addAttribute("posts",listDTO);
+        PagerModel pager = new PagerModel(listDTO.getTotalPages(),listDTO.getNumber(),Constant.BUTTONS_TO_SHOW);
         model.addAttribute("selectedPageSize", evalPageSize);
         model.addAttribute("pageSizes", Constant.PAGE_SIZES);
         model.addAttribute("pager", pager);
-
         return "index";
     }
 

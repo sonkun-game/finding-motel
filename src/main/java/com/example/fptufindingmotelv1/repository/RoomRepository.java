81fff2f1188aa83ccd76f2486f2412d1d50488f6
truongnqse05461 <truong10011998@gmail.com>
2020-07-08 22:29:31 +0700

    edit room ID

--- src/main/java/com/example/fptufindingmotelv1/repository/RoomRepository.java
+++ src/main/java/com/example/fptufindingmotelv1/repository/RoomRepository.java
@@ -3,5 +3,5 @@ package com.example.fptufindingmotelv1.repository;
 import com.example.fptufindingmotelv1.model.RoomModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoomRepository extends JpaRepository<RoomModel, Long> {
+public interface RoomRepository extends JpaRepository<RoomModel, String> {
 }

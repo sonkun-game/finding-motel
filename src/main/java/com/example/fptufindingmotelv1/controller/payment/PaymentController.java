7deeba890c8f3f5da8789dc55931bdbc790e9b1c
huylq4198 <huylq4198@gmail.com>
2020-07-28 15:09:34 +0700

    payment demo

--- src/main/java/com/example/fptufindingmotelv1/controller/payment/PaymentController.java
+++ src/main/java/com/example/fptufindingmotelv1/controller/payment/PaymentController.java
@@ -10,11 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class PaymentController {
@@ -25,23 +28,7 @@ public class PaymentController {
     LandlordRepository landlordRepository;
 
     @ResponseBody
-    @PostMapping(value = "/api-get-history-payment")
-    public List<PaymentDTO> getHistoryPaymentIntoAccount(){
-        if(SecurityContextHolder.getContext().getAuthentication() instanceof UsernamePasswordAuthenticationToken){
-            CustomUserDetails userDetails = (CustomUserDetails)SecurityContextHolder.getContext()
-                    .getAuthentication().getPrincipal();
-            LandlordModel landlordModel = landlordRepository.findByUsername(userDetails.getUsername());
-            List<PaymentDTO> response = new ArrayList<>();
-            for (PaymentModel paymet: landlordModel.getPaymentModels()) {
-                response.add(new PaymentDTO(paymet));
-            }
-            return response;
-        }
-        return null;
-    }
-
-    @ResponseBody
-    @PostMapping(value = "/api-get-history-payment-post")
+    @RequestMapping(value = "/api-get-history-payment-post")
     public List<PaymentPostDTO> getHistoryPaymentPost(){
         if(SecurityContextHolder.getContext().getAuthentication() instanceof UsernamePasswordAuthenticationToken){
             CustomUserDetails userDetails = (CustomUserDetails)SecurityContextHolder.getContext()
@@ -59,4 +46,62 @@ public class PaymentController {
         }
         return null;
     }
+
+    @RequestMapping(value = "/saveayment", method = RequestMethod.GET)
+    public String sentNotify(Model model, @RequestParam(value = "errorCode") String errorCode, @RequestParam(value = "localMessage") String localMess) {
+        return "redirect:/payment";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/get-momo-request", method = RequestMethod.POST)
+    public MomoModel sentRequestMomo(@RequestBody String amountValue) {
+        String partnerCode = "MOMO1J5T20200521";
+        String accessKey = "Y09NiKaRm3Utzc6x";
+        String requestType = "captureMoMoWallet";
+        String requestId = createUniquieID();
+        String amount = amountValue;
+        String orderId = "lehuy113";
+        String orderInfo = "Momo pay for user";
+        String returnUrl = "http://localhost:8080/notifyPayStatus";
+        String notifyUrl = "http://localhost:8080/notifyPayStatus";
+        String extraData = "";
+        String rawSign = "partnerCode=" + partnerCode + "&accessKey=" + accessKey
+                + "&requestId=" + requestId + "&amount=" + amount + "&orderId=" + orderId
+                + "&orderInfo=" + orderInfo + "&returnUrl=" + returnUrl + "&notifyUrl=" + notifyUrl
+                + "&extraData=" + extraData;
+
+        String signature = getSignature(rawSign);
+        MomoModel momoModel = new MomoModel(partnerCode, accessKey, requestType, requestId, amount, orderId, orderInfo
+                , returnUrl, notifyUrl, extraData, signature);
+        return momoModel;
+    }
+
+    public String getSignature(String rawSign) {
+        String key = "krEnQQAOs9oQexSsmbCETozJyTR6Gni9";
+        try {
+            Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+            sha256_HMAC.init(new SecretKeySpec(key.getBytes(), "HmacSHA256"));
+            byte[] result = sha256_HMAC.doFinal(rawSign.getBytes());
+            return bytesToHex(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private static String bytesToHex(byte[] hash) {
+        StringBuffer hexString = new StringBuffer();
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if (hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    private String createUniquieID() {
+        return "ffm" + UUID.randomUUID().toString();
+    }
+
 }
--- src/main/java/com/example/fptufindingmotelv1/repository/PaymentRepository.java
+++ src/main/java/com/example/fptufindingmotelv1/repository/PaymentRepository.java
@@ -4,4 +4,5 @@ import com.example.fptufindingmotelv1.model.PaymentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<PaymentModel, String> {
+
 }

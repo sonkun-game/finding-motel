906c4051be5361691e810c1f73576e69643ba6c3
huylq4198 <huylq4198@gmail.com>
2020-08-17 15:52:04 +0700

    test query user admin

--- src/main/java/com/example/fptufindingmotelv1/repository/UserRepository.java
+++ src/main/java/com/example/fptufindingmotelv1/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.fptufindingmotelv1.repository;
 
+import com.example.fptufindingmotelv1.dto.UserDTO;
 import com.example.fptufindingmotelv1.model.UserModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,17 +11,27 @@ import java.util.ArrayList;
 
 public interface UserRepository extends JpaRepository<UserModel, String> {
     UserModel findByGgAccount(String ggAccount);
+
     UserModel findByFbAccount(String fbAccount);
+
     UserModel findByUsername(String username);
+
     UserModel findByPhoneNumber(String phoneNumber);
 
     Boolean existsByUsername(String username);
+
     Boolean existsByPhoneNumber(String phone);
+
     Boolean existsByGgAccount(String ggAccount);
+
     Boolean existsByFbAccount(String fbAccount);
 
-    @Query("select u from UserModel u" +
+    @Query(value = "select new com.example.fptufindingmotelv1.dto.UserDTO(u) from UserModel u" +
             " where (u.username like %:username% or u.displayName like %:username%) " +
-            "and (:roleId is null or u.role.id = :roleId)")
-    Page<UserModel> searchUser(String username, Long roleId, Pageable pageable);
+            "and (:roleId is null or u.role.id = :roleId)"
+            , countQuery = "select count(u) from UserModel u" +
+            " where (u.username like %:username% or u.displayName like %:username%) " +
+            "and (:roleId is null or u.role.id = :roleId)"
+            , nativeQuery = false)
+    Page<UserDTO> searchUser(String username, Long roleId, Pageable pageable);
 }

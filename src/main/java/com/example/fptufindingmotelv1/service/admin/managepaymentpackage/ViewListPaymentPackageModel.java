b7155951c244da1dacd14ae7d6ab990588443375
truongnqse05461 <truong10011998@gmail.com>
2020-08-26 00:46:29 +0700

    update naming class model

---
+++ src/main/java/com/example/fptufindingmotelv1/service/admin/managepaymentpackage/ViewListPaymentPackageModel.java
@@ -0,0 +1,27 @@
+package com.example.fptufindingmotelv1.service.admin.managepaymentpackage;
+
+import com.example.fptufindingmotelv1.model.PaymentPackageModel;
+import com.example.fptufindingmotelv1.repository.PaymentPackageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ViewListPaymentPackageModel implements ViewListPaymentPackageService {
+    @Autowired
+    PaymentPackageRepository paymentPackageRepository;
+
+    @Override
+    public Page<PaymentPackageModel> getListPaymentPackage(Pageable pageable) {
+        try {
+            return paymentPackageRepository.getAllPaymentPackage(pageable);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/com/example/fptufindingmotelv1/service/renter/report/ReportAPostModel.java
@@ -0,0 +1,39 @@
+package com.example.fptufindingmotelv1.service.renter.report;
+
+import com.example.fptufindingmotelv1.dto.ReportRequestDTO;
+import com.example.fptufindingmotelv1.model.PostModel;
+import com.example.fptufindingmotelv1.model.RenterModel;
+import com.example.fptufindingmotelv1.model.ReportModel;
+import com.example.fptufindingmotelv1.model.StatusModel;
+import com.example.fptufindingmotelv1.repository.PostRepository;
+import com.example.fptufindingmotelv1.repository.RenterRepository;
+import com.example.fptufindingmotelv1.repository.ReportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportAPostModel implements ReportAPostService {
+
+    @Autowired
+    ReportRepository reportRepository;
+
+    @Autowired
+    RenterRepository renterRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Override
+    public void sendReport(ReportRequestDTO reportRequestDTO) {
+        try {
+            ReportModel reportModel = null;
+            PostModel postModel = postRepository.getOne(reportRequestDTO.getPostId());
+            RenterModel renterModel = renterRepository.getOne(reportRequestDTO.getRenterId());
+            StatusModel statusModel = new StatusModel(3L);
+            reportModel = new ReportModel(renterModel, postModel, statusModel, reportRequestDTO);
+            reportRepository.save(reportModel);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

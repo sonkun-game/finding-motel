8cca01355b5dc7fa2eca98bdf1347510c3cbef11
huylq4198 <huylq4198@gmail.com>
2020-09-02 16:51:06 +0700

    fix vnpay không cần chọn bank

--- src/main/java/com/example/fptufindingmotelv1/controller/landlord/PayIntoAccountController.java
+++ src/main/java/com/example/fptufindingmotelv1/controller/landlord/PayIntoAccountController.java
@@ -41,14 +41,14 @@ public class PayIntoAccountController {
         return payIntoAccountService.requestVnpayPayment(vnpayRequestDTO);
     }
 
-    @ResponseBody
     @RequestMapping(value = "/api-check-sum-vnpay-payment")
-    public JSONObject checkSumVnpayPayment(@RequestParam String vnp_TmnCode, @RequestParam Long vnp_Amount
-            , @RequestParam String vnp_BankCode, @RequestParam String vnp_BankTranNo, @RequestParam String vnp_CardType
-            , @RequestParam String vnp_PayDate, @RequestParam String vnp_OrderInfo
-            , @RequestParam String vnp_TransactionNo, @RequestParam String vnp_ResponseCode, @RequestParam String vnp_TxnRef
-            , @RequestParam String vnp_SecureHashType, @RequestParam String vnp_SecureHash) {
+    public String checkSumVnpayPayment(@RequestParam(required = false) String vnp_TmnCode, @RequestParam(required = false) Long vnp_Amount
+            , @RequestParam(required = false) String vnp_BankCode, @RequestParam(required = false) String vnp_BankTranNo, @RequestParam(required = false) String vnp_CardType
+            , @RequestParam(required = false) String vnp_PayDate, @RequestParam(required = false) String vnp_OrderInfo
+            , @RequestParam(required = false) String vnp_TransactionNo, @RequestParam(required = false) String vnp_ResponseCode, @RequestParam(required = false) String vnp_TxnRef
+            , @RequestParam(required = false) String vnp_SecureHashType, @RequestParam(required = false) String vnp_SecureHash) {
         VnpayResponseDTO vnpayResponseDTO = new VnpayResponseDTO(vnp_TmnCode, vnp_Amount,vnp_BankCode,vnp_BankTranNo,vnp_CardType,vnp_PayDate,vnp_OrderInfo,vnp_TransactionNo,vnp_ResponseCode,vnp_TxnRef,vnp_SecureHashType,vnp_SecureHash);
-        return payIntoAccountService.validateAndSaveVnpayPayment(vnpayResponseDTO);
+         payIntoAccountService.validateAndSaveVnpayPayment(vnpayResponseDTO);
+        return "profile-landlord";
     }
 }
